202c99bf894921205e9e2e6dc6ca266e0d9a0b9c
hejonable <hejon.etc@gmail.com>
2024-07-01 09:07:53 +0900

    Initial Commit

---
+++ calculator_final/calculator_finalApp.swift
@@ -0,0 +1,17 @@
+//
+//  calculator_finalApp.swift
+//  calculator_final
+//
+//  Created by kookie on 7/1/24.
+//
+
+import SwiftUI
+
+@main
+struct calculator_finalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
